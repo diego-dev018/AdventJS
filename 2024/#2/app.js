3e5b02779e99ad212a3a6b54f33377b8c24e3f5e
Diego Ronaldo Sanchez <diegosanchez.dev018@gmail.com>
2024-12-01 19:38:24 -0600

    Solution two easier

--- 2024/#2/app.js
+++ 2024/#2/app.js
@@ -1,14 +1,10 @@
 function createFrame(names) {
-    let length_spaces = 0
-    for (let i of names) {
-        if (i.length > length_spaces) {
-            length_spaces = i.length
-        }
-    }
+    let length_spaces = names.toSorted((a, b) => a.length - b.length).at(-1).length
+    const border = '*'.repeat(length_spaces + 4)
     return `\
-${'*'.repeat(length_spaces + 4)}
+${border}
 ${names.map(name => `* ${name}${' '.repeat(length_spaces - name.length + 1)}*`).join('\n')}
-${'*'.repeat(length_spaces + 4)}`
+${border}`
 }
 
 const frame = createFrame(['midu', 'madeval', 'educalvolpz'])
