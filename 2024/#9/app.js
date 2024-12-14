5289986457bac35553705adbc590a2603df4e723
Diego Ronaldo Sanchez <diegosanchez.dev018@gmail.com>
2024-12-14 04:11:59 -0600

    2024-NineAnswer-1Star

--- 2024/#9/app.js
+++ 2024/#9/app.js
@@ -8,21 +8,13 @@ function moveTrain(board, mov) {
             }
         }
     }
-    switch (mov) {
-        case 'L':
-            pos.x--
-            break;
-        case 'R':
-            pos.x++
-            break;
-        case 'U':
-            pos.y--
-            break;
-        case 'D':
-            pos.y++
-            break;
-        default:
-            break;
+
+    const move_y = { U: -1, D: 1 }
+    const move_x = { L: -1, R: 1 }
+    if (mov === 'R' || mov === 'L') {
+        pos.x = parseInt(pos.x) + (move_x[mov])
+    } else {
+        pos.y = parseInt(pos.y) + (move_y[mov])
     }
 
     if (pos.x < 0 || pos.y < 0 || pos.x >= board[0].length || pos.y >= board.length || board[pos.y][pos.x] === 'o') {
