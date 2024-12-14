c713b88198c75d3354b3b99a8cd0fb1fb6e982cc
Diego Ronaldo Sanchez <diegosanchez.dev018@gmail.com>
2024-12-14 04:36:26 -0600

    2024-NineAnswer-5Stars

--- 2024/#9/app.js
+++ 2024/#9/app.js
@@ -1,34 +1,17 @@
 function moveTrain(board, mov) {
-    let pos = { x: 0, y: 0 }
-    for (let i in board) {
-        for (let n in board[i]) {
-            if (board[i][n] === '@') {
-                pos.y = i
-                pos.x = n
-            }
-        }
-    }
+    const result = { '·': 'none', '*': 'eat' }
 
-    const move_y = { U: -1, D: 1 }
-    const move_x = { L: -1, R: 1 }
-    if (mov === 'R' || mov === 'L') {
-        pos.x = parseInt(pos.x) + (move_x[mov])
-    } else {
-        pos.y = parseInt(pos.y) + (move_y[mov])
-    }
-
-    try {        
-        const fObj = board[pos.y][pos.x]
-        if (fObj === 'o') {
-            return 'crash'
-        } else if (fObj === '*') {
-            return 'eat'
-        }
-        return 'none'
-    } catch {
-        return 'crash'
-    }
+    let y = board.findIndex(b => b.includes('@'))
+    let x = board[y].indexOf('@');
+  
+    if (mov == 'L') x--;
+    else if (mov == 'R') x++;
+    else if (mov == 'U') y--;
+    else y++;
+    const pos = board[y]?.[x]
+    return result[pos] ?? 'crash'
 }
+
 
 const board = [
     '·····',
